fbf03196f746dfb873d84f3fac379e7ef59c416e
Your Name <you@example.com>
2022-09-04 01:59:00 +0200

    ...

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Dto\ContactDto;
+use App\Dto\FeedbackDto;
+use App\Entity\Contact;
 use App\Entity\Property;
 use App\Repository\FilterRepository;
 use App\Repository\PropertyRepository;
@@ -73,7 +75,7 @@ final class PropertyController extends BaseController
     }
 
     /**
-     * @Route("/{citySlug}/{slug}/{id<\d+>}", methods={"GET"}, name="property_show")
+     * @Route("/{citySlug}/{slug}/{id<\d+>}", methods={"GET|POST"}, name="property_show")
      * @IsGranted("PROPERTY_VIEW", subject="property", message="Properties can only be shown to their owners.")
      */
     public function propertyShow(Request $request, URLService $url, Property $property, SimilarRepository $repository): Response
@@ -82,6 +84,14 @@ final class PropertyController extends BaseController
             return $this->redirect($url->generateCanonical($property), 301);
         } elseif ($url->isRefererFromCurrentHost($request)) {
             $show_back_button = true;
+        }
+
+
+        if($request->isMethod('POST'))
+        {
+            $this->addFlash('info','Mensage Ha sido envia correctamente');
+
+            $this->saveData($request->request->all());
         }
 
         return $this->render('property/show.html.twig',
@@ -93,5 +103,21 @@ final class PropertyController extends BaseController
                 'show_back_button' => $show_back_button ?? false,
             ]
         );
+    }
+
+    function saveData($data){
+
+        $contact = new Contact();
+
+        $contact->setEmail($data['email']);
+        $contact->setFullname($data['name']);
+        $contact->setLegal(true);
+        $contact->setTelefon($data['phone']);
+        $contact->setContent($data['url'] . "\r" .  $data['message'] );
+        $em =$this->doctrine->getManager();
+        $em->persist($contact);
+        $em->flush();
+
+
     }
 }
